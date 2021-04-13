833c652854ebb74aa68679b5b22469b86a2717c7
willy <guillermoaldaz@guillermos-MacBook-Pro.local>
2021-04-12 21:51:37 -0700

    practice

--- getData0.jsx
+++ getData0.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  const handler = (e) => alert(`button: ${e.target.getAttribute(index)}`);
+  const handler = (e) => alert(`button: ${e.target.getAttribute("index")}`);
   let a = [1,2,3,4,5]
   let list = a.map((item, index) => <MyButton index= {index}key={item} onClick={handler}></MyButton>)
 
